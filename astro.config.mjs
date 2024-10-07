8236408b994443e7093b371a03186c11001b296c
ZDibLO <101671735+ZDibLO@users.noreply.github.com>
2024-10-07 23:30:11 +0200

    Add SmartClicking

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,6 +60,7 @@ export default defineConfig({
                         {
                             label: 'Legit',
                             items: [
+                                { slug: 'legit/smartclick' }
                             ],
                         },
                         {
